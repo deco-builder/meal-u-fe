90f6f77d7f1dabf6f569366e8d876b5691ac558c
ammarashshiddiq2 <ammarashshiddiq2@gmail.com>
2024-09-06 02:07:30 +1000

    feat: add 2 more tabs

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,11 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Tab1 from './pages/Tab-1/Tab1';
+import Tab2 from './pages/Tab-2/Tab2';
+import Tab3 from './pages/Tab-3/Tab3';
+import Tab4 from './pages/Tab-4/Tab4';
+import Tab5 from './pages/Tab-5/Tab5';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -61,6 +63,12 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/tab4">
+            <Tab4 />
+          </Route>
+          <Route path="/tab5">
+            <Tab5 />
+          </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
@@ -77,6 +85,14 @@ const App: React.FC = () => (
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
             <IonLabel>Tab 3</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon aria-hidden="true" icon={square} />
+            <IonLabel>Tab 4</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab5" href="/tab5">
+            <IonIcon aria-hidden="true" icon={square} />
+            <IonLabel>Tab 5</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
