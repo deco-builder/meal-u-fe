8b82c4420597059e1c944a7e3dcf1667ea14c16c
azrahisman <azrahisman@gmail.com>
2024-09-29 20:37:23 +1000

    tweak: removed button

--- src/pages/MyCart/MyCart-Mobile/index.tsx
+++ src/pages/MyCart/MyCart-Mobile/index.tsx
@@ -2,7 +2,6 @@ import Cart from "../Cart";
 import Checkout from "../Checkout";
 import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React, { useState } from "react";
-import BottomButton from "../Cart/bottomButton";
 
 const MyCartMobile: React.FC = () => {
   const [subTotal, setSubTotal] = useState(0);
@@ -20,7 +19,6 @@ const MyCartMobile: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
         <Cart subTotal={subTotal} setSubTotal={setSubTotal}/>
-        {/* <BottomButton title="Proceed to Checkout" /> */}
         <Checkout subTotal={subTotal} total={total} setTotal={setTotal}/>
       </IonContent>
     </IonPage>
