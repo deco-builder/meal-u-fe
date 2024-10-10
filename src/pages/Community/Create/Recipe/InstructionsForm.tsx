db3901399b3436e0773de21551d26d21fb87f974
azrahisman <azrahisman@gmail.com>
2024-10-10 23:11:28 +1000

    feat: added reorder functionality in intructions form in create recipe

--- src/pages/Community/Create/Recipe/InstructionsForm.tsx
+++ src/pages/Community/Create/Recipe/InstructionsForm.tsx
@@ -1,8 +1,22 @@
 import { Dispatch, useEffect, useState } from "react";
 import { RecipeAction } from './index';
 import { CreateRecipePayload } from "../../../../api/recipeApi";
-import { IonButton, IonIcon, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonText } from "@ionic/react";
-import { trash } from 'ionicons/icons';
+import {
+  IonButton,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonReorder,
+  IonReorderGroup,
+  ItemReorderEventDetail,
+  IonText
+} from "@ionic/react";
+import { toggle, trash } from 'ionicons/icons';
 
 interface InstructionsFormProps {
   state: CreateRecipePayload;
@@ -11,21 +25,18 @@ interface InstructionsFormProps {
 
 const InstructionsForm: React.FC<InstructionsFormProps> = ({ state, dispatch }) => {
   const [instructions, setInstructions] = useState<string[]>([""]); // Start with one empty instruction
+  const [toggleDisabled, setToggleDisabled] = useState(true);
+  const [instructionsLength, setInstructionsLength] = useState(0);
+
+  useEffect(() => {
+    setInstructionsLength(instructions.length);
+  }, [instructions])
+
 
   useEffect(() => {
     // Sync with global state to save changes even when navigating through pages
     setInstructions(state.recipe.instructions.length ? state.recipe.instructions : [""]);
   }, [state.recipe.instructions]);
-
-  // Handle input change for a specific instruction
-  // const handleChange = (index: number, value: string) => {
-  //   const newInstructions = [...instructions];
-  //   newInstructions[index] = value;
-  //   setInstructions(newInstructions);
-    
-  //   // Update the state in the parent component
-  //   dispatch({ type: 'SET_FIELD', field: 'instructions', value: newInstructions });
-  // };
 
   // Handle input change for a specific instruction
   const handleChange = (index: number, value: any) => {
@@ -49,34 +60,54 @@ const InstructionsForm: React.FC<InstructionsFormProps> = ({ state, dispatch }) 
     setInstructions(instructions.filter((array) => array !== instructions[id]));
   }
 
+  const toggleReorder = () => {
+    setToggleDisabled(!toggleDisabled);
+  }
+
+  const handleReorder = (event: CustomEvent<ItemReorderEventDetail>) => {
+    setInstructions(event.detail.complete(instructions));
+    console.log("handleReorder: ", event.detail.complete(instructions));
+    event.detail.complete();
+  }
+
+  useEffect(() => {
+    if (instructions.length <= 1) {
+      setToggleDisabled(true);
+    }
+  }, [instructions.length])
+
   return (
     <div className="w-full max-w-md mx-auto">
       <div className="flex flex-col space-y-4 justify-start items-start mb-4">
         <IonLabel className="block text-gray-700 text-sm font-bold mb-2">
           Recipe Instructions
         </IonLabel>
-        {instructions.map((instruction, index) => (
-          <div key={index} className="w-full">
-            <IonItemSliding>
-              <IonItem>
-                <IonInput
-                  id={`instruction-${index}`} 
-                  type="text"
-                  value={instruction}
-                  placeholder="Add instructions for your recipe"
-                  onIonInput={(e) => handleChange(index, e.target.value)}
-                  label={`${index + 1}`}>
-                </IonInput>
-              </IonItem>
-              <IonItemOptions slot="end">
-                <IonItemOption color="danger" expandable={true} onClick={() => handleDelete(index)}>
-                  <IonIcon slot="icon-only" icon={trash}></IonIcon>
-                </IonItemOption>
-              </IonItemOptions>
-            </IonItemSliding>
-          </div>
-        ))}
-        {/* Center the "Add Instruction" button */}
+        <IonList>
+        <div className="w-full">
+        <IonReorderGroup disabled={toggleDisabled} onIonItemReorder={handleReorder}>
+          {instructions.map((instruction, index) => (
+              <IonItemSliding key={index}>
+                <IonItem>
+                  <IonInput
+                    id={`instruction-${index}`} 
+                    type="text"
+                    value={instruction}
+                    placeholder="Add instructions for your recipe"
+                    onIonInput={(e) => handleChange(index, e.target.value)}
+                    label={`${index + 1}`}>
+                  </IonInput>
+                  <IonReorder slot="end"></IonReorder>
+                </IonItem>
+                <IonItemOptions slot="end">
+                  <IonItemOption color="danger" expandable={true} onClick={() => handleDelete(index)}>
+                    <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                  </IonItemOption>
+                </IonItemOptions>
+              </IonItemSliding>
+          ))}
+        </IonReorderGroup>
+        </div>
+        </IonList>
         <div className="w-full flex justify-center">
           <IonText><i>Swipe left on an instruction to delete</i></IonText>
         </div>
@@ -88,6 +119,13 @@ const InstructionsForm: React.FC<InstructionsFormProps> = ({ state, dispatch }) 
           >
             Add Instruction
           </IonButton>
+          {instructionsLength > 1 ? (
+            <IonButton
+              expand="block"
+              onClick={toggleReorder}>
+                Reorder
+            </IonButton>
+          ) : null}
         </div>
       </div>
     </div>
