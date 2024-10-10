f37550cfcd7aba6759f0fde7466ab497ba5346ab
azrahisman <azrahisman@gmail.com>
2024-10-10 21:09:35 +1000

    feat: functionality to delete an instruction on create recipe

--- src/pages/Community/Create/Recipe/InstructionsForm.tsx
+++ src/pages/Community/Create/Recipe/InstructionsForm.tsx
@@ -1,6 +1,8 @@
-import { Dispatch, useState } from "react";
+import { Dispatch, useEffect, useState } from "react";
 import { RecipeAction } from './index';
 import { CreateRecipePayload } from "../../../../api/recipeApi";
+import { IonButton, IonIcon, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonText } from "@ionic/react";
+import { trash } from 'ionicons/icons';
 
 interface InstructionsFormProps {
   state: CreateRecipePayload;
@@ -10,15 +12,31 @@ interface InstructionsFormProps {
 const InstructionsForm: React.FC<InstructionsFormProps> = ({ state, dispatch }) => {
   const [instructions, setInstructions] = useState<string[]>([""]); // Start with one empty instruction
 
+  useEffect(() => {
+    // Sync with global state to save changes even when navigating through pages
+    setInstructions(state.recipe.instructions.length ? state.recipe.instructions : [""]);
+  }, [state.recipe.instructions]);
+
   // Handle input change for a specific instruction
-  const handleChange = (index: number, value: string) => {
+  // const handleChange = (index: number, value: string) => {
+  //   const newInstructions = [...instructions];
+  //   newInstructions[index] = value;
+  //   setInstructions(newInstructions);
+    
+  //   // Update the state in the parent component
+  //   dispatch({ type: 'SET_FIELD', field: 'instructions', value: newInstructions });
+  // };
+
+  // Handle input change for a specific instruction
+  const handleChange = (index: number, value: any) => {
     const newInstructions = [...instructions];
-    newInstructions[index] = value;
+    newInstructions[index] = value || ""; // Extract the value from IonInput's event
     setInstructions(newInstructions);
     
     // Update the state in the parent component
     dispatch({ type: 'SET_FIELD', field: 'instructions', value: newInstructions });
   };
+
 
   // Add a new input field if all current fields are filled
   const addNewInstruction = () => {
@@ -27,25 +45,41 @@ const InstructionsForm: React.FC<InstructionsFormProps> = ({ state, dispatch }) 
     }
   };
 
+  const handleDelete = (id: number) => {
+    setInstructions(instructions.filter((array) => array !== instructions[id]));
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <div className="flex flex-col space-y-4 justify-start items-start mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
+        <IonLabel className="block text-gray-700 text-sm font-bold mb-2">
           Recipe Instructions
-        </label>
+        </IonLabel>
         {instructions.map((instruction, index) => (
           <div key={index} className="w-full">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id={`instruction-${index}`}
-              type="text"
-              value={instruction}
-              placeholder={`Instruction ${index + 1}`}
-              onChange={(e) => handleChange(index, e.target.value)}
-            />
+            <IonItemSliding>
+              <IonItem>
+                <IonInput
+                  id={`instruction-${index}`} 
+                  type="text"
+                  value={instruction}
+                  placeholder="Add instructions for your recipe"
+                  onIonInput={(e) => handleChange(index, e.target.value)}
+                  label={`${index + 1}`}>
+                </IonInput>
+              </IonItem>
+              <IonItemOptions slot="end">
+                <IonItemOption color="danger" expandable={true} onClick={() => handleDelete(index)}>
+                  <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
           </div>
         ))}
         {/* Center the "Add Instruction" button */}
+        <div className="w-full flex justify-center">
+          <IonText><i>Swipe left on an instruction to delete</i></IonText>
+        </div>
         <div className="w-full flex justify-center">
           <button
             className={`mt-4 px-4 py-2 rounded bg-blue-500 text-white ${!instructions.every(instruction => instruction.trim() !== "") ? "opacity-50 cursor-not-allowed" : ""}`}
